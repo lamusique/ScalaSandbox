631edb8b233f809f0b6e1f2d87cc833ac9eeba11
La Musique <lamusique@users.noreply.github.com>
2013-07-22 14:08:52 +0900

    make sure of date range

--- ScalaTest/src/test/scala/com/nekopiano/scala/sandbox/IsoDateTimeTest.scala
+++ ScalaTest/src/test/scala/com/nekopiano/scala/sandbox/IsoDateTimeTest.scala
@@ -13,11 +13,21 @@ object IsoDateTimeTest {
 
   def main(args: Array[String]): Unit = {
 
+    // OK
+    parse("2013-05-31T13:32:31+08:00")
+    parse("20113-06-07T13:32:31+09:00")
+    parse("22001133-06-07T13:32:31+09:00")
+    // NG
+    // parse("22001133-106-07T13:32:31+09:00")    
+
+  }
+
+  def parse(utcDateTime: String) {
     import com.github.nscala_time.time.Imports._
 
     // nscala test, which is a wrapper for Joda-Time
     val XML_DATE_TIME_FORMAT = ISODateTimeFormat.dateTimeNoMillis()
-    val utcDateTime = "2013-05-31T13:32:31+08:00"
+
     // throw IllegalArgumentException
     val dt = XML_DATE_TIME_FORMAT.parseDateTime(utcDateTime)
     val date = dt.toDate
