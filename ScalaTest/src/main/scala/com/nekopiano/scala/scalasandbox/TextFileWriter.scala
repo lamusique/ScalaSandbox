c95def1f63d8cc95bbe64d3696094d79e8074f88
La Musique <lamusique@users.noreply.github.com>
2013-07-27 00:00:07 +0900

    added quick solution.

--- ScalaTest/src/main/scala/com/nekopiano/scala/scalasandbox/TextFileWriter.scala
+++ ScalaTest/src/main/scala/com/nekopiano/scala/scalasandbox/TextFileWriter.scala
@@ -3,6 +3,8 @@
  */
 package com.nekopiano.scala.scalasandbox
 
+import java.io.File
+
 /**
  * @author La Musique
  *
@@ -19,8 +21,16 @@ object TextFileWriter {
     //    "dir /b" #> new java.io.File("example.txt")
 
     //    val process = Process("cmd /C dir") !
-    "cmd /C echo hello world" #> new java.io.File("example.txt") !
+    "cmd /C echo hello world" #> new java.io.File("exampleA.txt") !
 
+    // Simple solution
+    //http://stackoverflow.com/questions/4604237/how-to-write-to-a-file-in-scala
+    val pw = new java.io.PrintWriter(new File("exampleB.txt"), "UTF-8")
+    try {
+      pw.write("Hello from PrintWriter. こんにちは。")
+    } finally {
+      pw.close()
+    }
   }
 
 }
