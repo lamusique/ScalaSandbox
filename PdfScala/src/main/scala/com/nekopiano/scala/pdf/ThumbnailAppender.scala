3538ccbc814619bf85461ba822cdc8b3aa04fd33
La Musique <lamusique@users.noreply.github.com>
2013-09-18 23:59:56 +0900

    set default view

--- PdfScala/src/main/scala/com/nekopiano/scala/pdf/ThumbnailAppender.scala
+++ PdfScala/src/main/scala/com/nekopiano/scala/pdf/ThumbnailAppender.scala
@@ -9,6 +9,12 @@ import org.apache.pdfbox.pdmodel.PDPage
 import javax.imageio.ImageIO
 import org.apache.pdfbox.pdmodel.common.PDMetadata
 import org.apache.pdfbox.pdmodel.PDDocumentCatalog
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDDestination
+import org.apache.pdfbox.cos.COSBase
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageFitHeightDestination
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageFitRectangleDestination
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageXYZDestination
+import org.apache.pdfbox.pdmodel.interactive.action.`type`.PDActionGoTo
 
 /**
  * @author La Musique
@@ -20,9 +26,26 @@ object ThumbnailAppender {
     val pagedrawer = new PageDrawer()
 
     val srcPdf = PDDocument.load("pdf\\inserted\\Dubois-Theory.pdf")
-    
+
     // just set default mode rather than appending thumbnails...
-    srcPdf.getDocumentCatalog.setPageMode(PDDocumentCatalog.PAGE_MODE_USE_THUMBS)
+    val dc = srcPdf.getDocumentCatalog
+        dc.setPageMode(PDDocumentCatalog.PAGE_MODE_USE_THUMBS)
+
+    val openAction = dc.getOpenAction
+    println("openAction=" + openAction)
+    val dest = new PDPageFitHeightDestination
+    //    val dest = new PDPageXYZDestination
+    //    dest.setZoom(15)
+    //    dest.setLeft(0)
+    //    dest.setTop(500)
+    // Action activation needs this.
+    dest.setPageNumber(0)
+    val action = new PDActionGoTo()
+    action.setDestination(dest)
+
+    dc.setOpenAction(dest)
+    println("dc.getOpenAction=" + dc.getOpenAction)
+    //    dc.setOpenAction(new PDPageFitRectangleDestination)
 
     srcPdf.save("pdf\\thumbnailed\\" + "Dubois-Theory-Thumbnailed.pdf")
     srcPdf.close
