25b10edd4c1e89492a4a4e695d678ea0ff210e68
La Musique <lamusique@users.noreply.github.com>
2013-09-19 11:07:00 +0900

    to make executable jar.

---
+++ ScalaTest/src/main/java/Entry.java
@@ -0,0 +1,14 @@
+import com.nekopiano.scala.scalasandbox.console.ProgressConsoleDemo;
+
+
+/**
+ * @author La Musique
+ * 
+ */
+public class Entry {
+
+	public static void main(String[] args) {
+		ProgressConsoleDemo.main(args);
+	}
+
+}
