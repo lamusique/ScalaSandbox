1fa6b7ddbc6b0b030dedeaac327856d1108a1867
La Musique <lamusique@users.noreply.github.com>
2013-09-19 11:08:23 +0900

    to make executable jar.

---
+++ ScalaMavenRest/src/main/java/Entry.java
@@ -0,0 +1,13 @@
+import com.nekopiano.scala.rest.client.RestClient;
+
+/**
+ * @author La Musique
+ * 
+ */
+public class Entry {
+
+	public static void main(String[] args) {
+		RestClient.main(args);
+	}
+
+}
