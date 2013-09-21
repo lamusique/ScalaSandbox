23d46e330551ffc1bb64f36fc8e7746387a89649
La Musique <lamusique@users.noreply.github.com>
2013-09-21 21:18:29 +0900

    make pdf file name into a value.

--- PdfScala/src/main/scala/com/nekopiano/scala/pdf/SpecificInverter.scala
+++ PdfScala/src/main/scala/com/nekopiano/scala/pdf/SpecificInverter.scala
@@ -12,18 +12,18 @@ import org.apache.pdfbox.pdmodel.PDPage
  */
 object SpecificInverter {
 
-//  val pdfFilePath = raw"C:\Users\La Musique\Pictures\Toolbox\デュボア-和声学理論編\edit\merged1.pdf"
-  val pageNumbers = Seq(13, 19, 77, 125, 204, 270, 289, 298, 299)
+  //  val pdfFilePath = raw"C:\Users\La Musique\Pictures\Toolbox\デュボア-和声学理論編\edit\merged1.pdf"
+  val pageNumbers = Seq(21, 35, 57, 61, 63, 69, 71, 73, 95, 117, 127, 129, 131)
 
   def main(args: Array[String]): Unit = {
 
     import scala.collection.JavaConverters._
 
-    val pdfFileName = "Dubois-Theory-inverted-1.pdf"
+    val pdfFileName = "Dubois-Realization.pdf"
     val readPdf = PDDocument.load("pdf\\" + pdfFileName)
     val pageList = readPdf.getDocumentCatalog.getAllPages.asScala.toList.asInstanceOf[List[PDPage]]
     println("pagelist.size=" + pageList.size)
-    val newPdf: PDDocument = new PDDocument()
+    val newPdf = new PDDocument
     pageList.zipWithIndex.foreach {
       case (page, i) => {
         // -15 doesn't work...
@@ -31,7 +31,7 @@ object SpecificInverter {
         newPdf.addPage(page)
       }
     }
-    newPdf.save("pdf\\inverted\\" + "Dubois-Theory-inverted.pdf")
+    newPdf.save("pdf\\inverted\\" + pdfFileName)
     newPdf.close
     println("Finished.")
   }
