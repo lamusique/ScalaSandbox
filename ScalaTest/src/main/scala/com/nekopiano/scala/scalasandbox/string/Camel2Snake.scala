092a0c569c9a780be11c305df4b912cf35d5d2af
La Musique <lamusique@users.noreply.github.com>
2014-03-28 14:33:40 +0900

    format

--- ScalaTest/src/main/scala/com/nekopiano/scala/scalasandbox/string/Camel2Snake.scala
+++ ScalaTest/src/main/scala/com/nekopiano/scala/scalasandbox/string/Camel2Snake.scala
@@ -52,13 +52,12 @@ importDate
     "_" + m.group(0).toLowerCase()
   })
 
-  /* 
- * Takes an underscore separated identifier name and returns a camel cased one
- *
- * Example:
- *    underscoreToCamel("this_is_a_1_test") == "thisIsA1Test"
- */
-
+  /**
+   * Takes an underscore separated identifier name and returns a camel cased one
+   *
+   * Example:
+   *    underscoreToCamel("this_is_a_1_test") == "thisIsA1Test"
+   */
   def underscoreToCamel(name: String) = "_([a-z\\d])".r.replaceAllIn(name, { m =>
     m.group(1).toUpperCase()
   })
