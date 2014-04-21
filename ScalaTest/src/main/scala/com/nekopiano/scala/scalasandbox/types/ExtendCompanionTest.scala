3d382dcfb18f7f02a1f775f6ca3102c3d57494e5
La Musique <lamusique@users.noreply.github.com>
2014-04-21 19:02:51 +0900

    added the case tupled appears and doesn't.

--- ScalaTest/src/main/scala/com/nekopiano/scala/scalasandbox/types/ExtendCompanionTest.scala
+++ ScalaTest/src/main/scala/com/nekopiano/scala/scalasandbox/types/ExtendCompanionTest.scala
@@ -8,14 +8,28 @@ package com.nekopiano.scala.scalasandbox.types
  *
  */
 object ExtendCompanionTest {
-  
-  val pcc = PlainCaseClass("test")
-  val a = PlainCaseClass.apply("a")
 
-  
+  val pcc = OneArgCaseClass("test")
+  val a = OneArgCaseClass.apply("a")
+
+  //  val tupledOne = OneArgCaseClass.tupled
+  // compile error owing to one arg
+
+  val tupledTwo = TwoArgsCaseClass.tupled
+
+  case class Person(name: String, age: Int)
+  // def apply(name: String, age: Int): Personを
+  // def f(tuple: (String, Int)): Personのように変換する
+  val f = Person.tupled
+  val tuple = ("Taro", 30)
+  f(tuple)
+
 }
 
-case class PlainCaseClass(arg:String) {}
-object PlainCaseClass {
-  val constant = "xxx"
-}
+case class OneArgCaseClass(arg: String) {}
+//object PlainCaseClass {
+//  val constant = "xxx"
+//}
+case class TwoArgsCaseClass(arg1: String, arg2: String) {}
+// object TwoArgsCaseClass {}
+// makes TwoArgsCaseClass.tupled have a compile error.
