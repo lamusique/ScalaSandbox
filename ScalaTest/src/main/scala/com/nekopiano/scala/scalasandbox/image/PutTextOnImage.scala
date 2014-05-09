d2a115e04e7c75ef2f9b0b4967cbaf52783f4e7c
La Musique <lamusique@users.noreply.github.com>
2014-05-09 14:28:28 +0900

    changed the size: 500 -> 300

--- ScalaTest/src/main/scala/com/nekopiano/scala/scalasandbox/image/PutTextOnImage.scala
+++ ScalaTest/src/main/scala/com/nekopiano/scala/scalasandbox/image/PutTextOnImage.scala
@@ -15,14 +15,18 @@ import java.awt.Color
  */
 object PutTextOnImage extends App {
 
-  (1 to 2000) foreach { i =>
+  util.Properties.setProp("scala.time", "")
+
+  //in parallel
+  (1 to 2000).par foreach { i =>
 
     // http://stackoverflow.com/questions/10929524/how-to-add-text-to-an-image-in-java
 
     //      final BufferedImage image = ImageIO.read(new URL(
     //        "http://upload.wikimedia.org/wikipedia/en/2/24/Lenna.png"));
     val image = ImageIO.read(new File(
-      raw"C:\Users\La Musique\Pictures\UploadSamples\Generator\img_bt1s1620_01-500Wx500H.jpg"));
+      // raw"C:\Users\La Musique\Pictures\UploadSamples\Generator\img_bt1s1620_01-500Wx500H.jpg"));
+      raw"C:\Users\La Musique\Pictures\UploadSamples\Generator\img_bt1s1620_01-300Wx300H.jpg"));
 
     // http://stackoverflow.com/questions/7550454/how-can-i-make-the-text-edges-in-the-images-smooth
     //  val g = image.getGraphics()
@@ -31,14 +35,17 @@ object PutTextOnImage extends App {
     g.setRenderingHint(
       RenderingHints.KEY_ALa MusiqueIALIASING,
       RenderingHints.VALUE_ALa MusiqueIALIAS_ON)
-    g.setFont(g.getFont().deriveFont(50f))
+    //    g.setFont(g.getFont().deriveFont(50f))
+    g.setFont(g.getFont().deriveFont(30f))
     g.setColor(Color.DARK_GRAY)
 
     val seq = "%05d".format(i)
-    g.drawString("Sample " + seq, 15, 190)
+    //    g.drawString("Sample " + seq, 15, 190)
+    g.drawString("Sample " + seq, 9, 117)
     g.dispose()
 
-    ImageIO.write(image, "jpg", new File(raw"C:\Users\La Musique\Pictures\UploadSamples\Generator\generated\sample-bath-" + seq + ".jpg"));
+    ImageIO.write(image, "jpg", new File(raw"C:\Users\La Musique\Pictures\UploadSamples\Generator\generated-300\sample-bath-" + seq + ".jpg"));
+    println(seq + " has been created.")
   }
   println("finished")
 }
